c4b5bfc54e321f288c5f58b1aaac73abd818c107
Ankush Sharma <ankush1@ualberta.ca>
2019-01-20 05:56:30 -0700

    fixed up some stuff

--- resources/views/partials/brochure/_head_login.blade.php
+++ resources/views/partials/brochure/_head_login.blade.php
@@ -2,9 +2,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
   <meta name="author" content="Creative Tim">
-  <title>Argon fix this </title>
+  <title>RapidRenter</title>
   <!-- Favicon -->
-  <link href="./dash/img/brand/favicon.png" rel="icon" type="image/png">
+  <div class="col-md-2">
+      <div id="colorlib-logo"><a href="index.html">{{ config('app.name') }}</a></div>
+  </div>
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
   <!-- Icons -->
